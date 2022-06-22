733109a55b3cda7f6106350a5e5bedfb40215ce6
mohammedkohil <107880802+mohammedkohil@users.noreply.github.com>
2022-06-22 03:23:00 +0300

    Add files via upload

---
+++ moha123.py
@@ -0,0 +1,23 @@
+name= "ali"
+age = "32"
+car = "RIO"
+print("i am "+name.upper() +" i am "+age+" year old" "\n my car is "+car.lower() +" and i am having fun")
+name = "omar"
+print (name.upper() .islower() )
+print(name.islower() )
+name = "khaled ahmad is out side"
+print (len(name))
+print (name[4])
+print (name.index("is") )
+print (name.replace("khaled","ali"))
+name = input("enter your name: ")
+age = input("enter your age:")
+print("hellow " + name + ",your age is " +age)
+reward =input("enter reward: ")
+rong = input("enter rong: ")
+swap = input("swap: ")
+result = int(reward) + float(rong) + float(swap)
+print(result)
+cars = ["kia", "honda"]
+speed = ["60", "70" , ]
+print(cars[0] + speed [1])
